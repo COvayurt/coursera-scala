4541080a7d5a239b6a2574e090c992b36fa83ed2
Marco Otte-Witte <mottewitte@rim.com>
2013-03-25 11:41:33 +0100

    added HelloWorld project

---
+++ HelloWorld/.worksheet/src/greeter.HelloSheet.scala
@@ -0,0 +1,5 @@
+package greeter
+
+object HelloSheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(80); 
+  println("Welcome to the Scala worksheet")}
+}

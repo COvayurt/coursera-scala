cb173a563821f30559106ef6d7844c2c35a718d1
Marco Otte-Witte <mottewitte@rim.com>
2013-05-02 16:43:02 +0200

    initial wrong version of combinations

--- assignments/5-forcomp/src/main/scala/forcomp/Anagrams.scala
+++ assignments/5-forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -81,7 +81,18 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] = occurrences match {
+    case List()                => List()
+    case (char, count) :: tail => {
+      (
+        for {
+          c    <- 1 to count
+          occ  <- List(List((char, c)))
+        } yield occ
+      ).toList
+    }
+  }
+    
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    * 
